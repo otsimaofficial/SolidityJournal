3f4a98ba59b8d119c218c382a3221a41f491cc7a
Emmanuel Ogbu <iemmanuelogbu@gmail.com>
2025-08-17 10:34:02 +0100

    test placholder added

---
+++ test/Counter.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+
+contract Counter is Test {
+    function test_Counter() public pure {
+        assertTrue(true, "contract test placeholder");
+    }
+}
